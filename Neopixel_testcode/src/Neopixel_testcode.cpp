b393d4319ec9279389faef0794bd832598f81c4b
Courtney Power <crpower05@gmail.com>
2024-04-25 21:46:38 -0600

    box breathing with array

--- Neopixel_testcode/src/Neopixel_testcode.cpp
+++ Neopixel_testcode/src/Neopixel_testcode.cpp
@@ -12,11 +12,17 @@
 #include "Colors.h"
 #include "IoTTimer.h"
 
-const int PIXELCOUNT = 25;
+// const int PIXELCOUNT = 25;
 const int BRI = 5;
-Adafruit_NeoPixel pixel(PIXELCOUNT, SPI1, WS2812B);
-int neoNum;
+// Adafruit_NeoPixel pixel(PIXELCOUNT, SPI1, WS2812B);
+
+#define PIXEL_COUNT 25
+#define PIXEL_TYPE WS2812B
+
+Adafruit_NeoPixel pixel(PIXEL_COUNT, SPI1, PIXEL_TYPE);
+int neoNum, currentTime, lastTime;
 IoTTimer boxBreathingTimer;
+int breathingArray[16] = {19, 14, 9, 4, 3, 2, 1, 0, 5, 10, 15, 20, 21, 22, 23, 24};
 
 // setup() runs once, when the device is first turned on
 void setup() {
@@ -25,16 +31,33 @@ void setup() {
   pixel.clear();
   pixel.show();
   neoNum = 0;
+  lastTime = millis();
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // box breathing
 
-boxBreathingTimer.startTimer(1000);
-if (boxBreathingTimer.isTimerReady()) {
-  pixel.setPixelColor(neoNum, teal);
+
+currentTime = millis();
+if (currentTime - lastTime > 1000) {
+// boxBreathingTimer.startTimer(1000);
+// if (boxBreathingTimer.isTimerReady()) {
+  pixel.setPixelColor(breathingArray[neoNum], teal);
   pixel.show();
 neoNum = neoNum + 1;
+// }
+lastTime = millis();
 }
+if (neoNum == 16) {
+  neoNum = 0;
+  pixel.clear();
+  pixel.show();
+}
+// for (neoNum = 0; neoNum < PIXEL_COUNT; neoNum ++) {
+//   pixel.setPixelColor (neoNum, green);
+//   pixel.show();
+//   }
+//   pixel.clear();
+//   pixel.show();
 }
