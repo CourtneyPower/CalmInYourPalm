211e194efe1011f7b3a8430adaeff653a6b5ca9a
Courtney Power <crpower05@gmail.com>
2024-05-03 09:37:48 -0600

    encoder and Racing Thoughts working

--- Neopixel_testcode/src/Neopixel_testcode.cpp
+++ Neopixel_testcode/src/Neopixel_testcode.cpp
@@ -30,10 +30,11 @@ Adafruit_SSD1306 display(OLED_RESET);
 Adafruit_NeoPixel pixel(PIXEL_COUNT, SPI1, PIXEL_TYPE);
 Encoder myEnc(D8, D9);
 Button encSwitch(D16);
+IoTTimer sessionTimer;
 int neoNum, currentTime, lastTime, i, j, k;
 bool onOff;
-int encPos, encToPixel, encMin, encMax, pixelMin, pixelMax;
-IoTTimer sessionTimer;
+int encPos, encToPixel, encMin, encMax, pixelMin, pixelMax, holdPixel;
+
 int breathingArray[16] = {19, 14, 9, 4, 3, 2, 1, 0, 5, 10, 15, 20, 21, 22, 23, 24};
 int breathingColors[4] = {blue, teal, blue, teal};
 int outsideSquare[16] = {19, 14, 9, 4, 3, 2, 1, 0, 5, 10, 15, 20, 21, 22, 23, 24};
@@ -41,6 +42,8 @@ int middleSquare[8] = {6, 7, 8, 11, 13, 16, 17, 18};
 int centerSquare[1] = {12};
 int centeringDelays[18] = {200, 350, 500, 650, 800, 950, 1100, 1250, 1400, 1550, 1700, 1850, 2000, 2200, 2400, 2600, 2800, 3000};
 int racingNumbers[25] = {4, 3, 2, 1, 0, 5, 6, 7, 8, 9, 14, 13, 12, 11, 10, 15, 16, 17, 18, 19, 24, 23, 22, 21, 20};
+int happyFace[9] = {3, 8, 1, 6, 14, 18, 17, 16, 10};
+
 enum miniPixel {
   OUTER,
   MIDDLE,
@@ -60,43 +63,42 @@ void readySetGo();
 // setup() runs once, when the device is first turned on
 void setup() {
 Serial.begin(9600);
-waitFor(Serial.isConnected, 15000);
+waitFor(Serial.isConnected, 10000);
   pixel.begin();
   pixel.setBrightness (BRI);
   pixel.clear();
   pixel.show();
   neoNum = 0;
-
- display.begin(SSD1306_SWITCHCAPVCC, 0x3c); //initialize OLED
- display.display();
- delay(1000);
- display.clearDisplay();
- display.display();
- display.setTextSize(2);
+  i = 0;
+display.begin(SSD1306_SWITCHCAPVCC, 0x3c); //initialize OLED
+display.display();
+delay(1000);
+display.clearDisplay();
+display.display();
+display.setTextSize(2);
 display.setTextColor(WHITE);
 display.setCursor(0,0);
   encPos = 0;
   encMin = 0;
   encMax = 95;
-  pixelMin = 0;
-  pixelMax = 24;
-  lastTime = millis();
-  sessionTimer.startTimer(60000);
+lastTime = millis();
+sessionTimer.startTimer(60000); //move into loop once modes fixed
 }
 
 void loop() {
 
+// // box breathing, pixel moves every 1 second
 // display.printf("Box\n"); //OLED screen
 // display.printf("Breathing\n");
 // display.display();
 // delay(1000);
 // display.clearDisplay();
-// display.display();
-// // box breathing, pixel moves every 1 second
+// // sessionTimer.startTimer(60000);
 // readySetGo();
+// display.display();
 // while(!sessionTimer.isTimerReady()){
-// currentTime = millis();
-// if (currentTime - lastTime > 1000) {
+// //currentTime = millis();
+// if (millis() - lastTime > 1000) {
 //   pixel.setPixelColor(breathingArray[neoNum], breathingColors[neoNum/4]);
 //   pixel.show();
 //     if ((neoNum % 4) == 3) {
@@ -118,12 +120,12 @@ void loop() {
 // display.display();
 // delay(1000);
 // display.clearDisplay();
-// display.display();
 // centering = OUTER;
 // i=0;
 // //j=0;
-// readySetGo();
 // // sessionTimer.startTimer(60000);
+// readySetGo();
+// display.display();
 // while(!sessionTimer.isTimerReady()){
 //  if ((millis() - lastTime) > centeringDelays[i%18]){
 //     lastTime = millis();
@@ -159,46 +161,72 @@ void loop() {
 //   }
 // }
 
+//RACING THOUGHTS
+sessionTimer.startTimer(60000);
+display.printf("Slow\n"); //OLED screen
+display.printf("Down\n");
+display.display();
+delay(1000);
+readySetGo();
+display.clearDisplay();
+display.display();
+i = 0;
+while(!sessionTimer.isTimerReady()){
+ if ((millis() - lastTime) > centeringDelays[i * 2]){
+  Serial.printf("Racing delay is %ims\n", centeringDelays[i*2]);
+  lastTime = millis();
 encPos = myEnc.read();
 Serial.printf("encoder position is %i\n", encPos);
 encToPixel = map(encPos, 0, 95, 0, 24);
-if (encPos < encMin) {
-  encPos = encMin;
-  myEnc.write(encMin);
-}
-if (encPos > encMax) {
-  encPos = encMax;
-  myEnc.write(encMax);
-}
-if (encToPixel < pixelMin) {
-  encToPixel = pixelMin;
-}
-if (encToPixel > pixelMax) {
-  encToPixel = pixelMax;
-}
-
-pixel.setPixelColor(racingNumbers[encToPixel],red);
-pixel.show();
-pixel.clear();
-pixel.show();
-
-
-
-// if (sessionTimer.isTimerReady()){
-//   pixel.clear();
-//   pixel.show();
-//   display.printf("Feel\n");
-//   display.printf("Better\n");
-//   display.display();
+    if (encToPixel != holdPixel) {
+      pixel.clear();
+      pixel.show();
+    }
+// if (encPos <= encMin) {
+//   encPos = encMax;
+//   myEnc.write(encMax);
 // }
+    if (encPos >= encMax) {
+      encPos = encMin;
+      myEnc.write(encMin);
+    }
+  pixel.clear();
+  pixel.setPixelColor(racingNumbers[encToPixel],red);
+    holdPixel = encToPixel;
+  pixel.setPixelColor(racingNumbers[neoNum], purple);
+    if (encToPixel == neoNum){
+      pixel.setPixelColor(racingNumbers[neoNum], green);
+    }
+  pixel.show();
+
+    if (neoNum == 24){
+      neoNum = 0;
+      i++;
+      if (i == 18){
+        i = 0;
+      }
+    }
+  neoNum = neoNum + 1;
+}
+}
+
+if (sessionTimer.isTimerReady()){
+  pixel.clear();
+for (i=0; i<9; i++) {
+  pixel.setPixelColor(happyFace[i],orange);
+}
+  pixel.show();
+  display.printf("Feel\n");
+  display.printf("Better\n");
+  display.display();
+}
 //// if switch is FALLING, transmit date/time/mode to mqtt, go to low power mode (ie: I'm done)
 ////if no input after 3 seconds, restart session timer for current mode, clear display
 // display.clearDisplay();
 // display.display();
 }
 
-void readySetGo() {
-    // ready set go green light
+void readySetGo() {     // ready set go green light
 int i;
 for (i=0; i<6; i++) {
   if ((i % 2) == 0) {
@@ -211,3 +239,4 @@ for (i=0; i<6; i++) {
   delay(300);
 }
 }
+
